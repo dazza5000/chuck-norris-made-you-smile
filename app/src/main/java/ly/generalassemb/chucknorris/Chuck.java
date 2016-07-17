7581ff5859d47833bcb1a4cbe8ae8a867541248b
Darran Kelinske <darran.kelinske@gmail.com>
2016-07-16 21:03:16 -0500

    first commit

---
+++ app/src/main/java/ly/generalassemb/chucknorris/Chuck.java
@@ -0,0 +1,16 @@
+package ly.generalassemb.chucknorris;
+
+/**
+ * Created by darrankelinske on 7/14/16.
+ */
+public class Chuck {
+    private String quote;
+
+    public Chuck(String quote) {
+        this.quote = quote;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+}
